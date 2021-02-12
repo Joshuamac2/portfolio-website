dedf25fe68a85c64735230166eebc80e900ef7b5
Joshua <joshuamacleod@live.com>
2021-02-12 15:01:17 +0000

    Contact form for contact page published

--- portfolio-website/src/pages/AboutPage.js
+++ portfolio-website/src/pages/AboutPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
     return(
-        <p>AboutPage Works!</p>
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hello friendly people</p>
+                <p>Hello friendly people</p>
+                <p>Hello friendly people</p>
+                <p>Hello friendly people</p>
+            </Content>
+        </div>
     );
 }
 
