ff7a86295177710ab8849278aa2e1706a13a5e39
Joshua <joshuamacleod@live.com>
2021-02-13 01:10:33 +0000

    Removed contact page

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# portfolio-webtsite
+My portfolio website
--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import './App.css';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
 
 
 
@@ -21,7 +20,6 @@ class App extends React.Component {
       headerLinks: [
         { title: 'Home', path: '/'},
         { title: 'About', path: '/about'},
-        { title: 'Contact', path: '/contact'}
       ],
       home: {
         title: 'My website',
@@ -50,14 +48,12 @@ class App extends React.Component {
                   <Nav className="ml-auto">
                     <Link className="nav-link" to="/">Home</Link>
                     <Link className="nav-link" to="/about">About</Link>
-                    <Link className="nav-link" to="/contact">Contact</Link>
                   </Nav>
                 </Navbar.Collapse>
               </Navbar>
 
               <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
               <Route path="/about" render={() => <AboutPage title={this.state.about.title}/>} />
-              <Route path="/contact" render={() => <ContactPage title={this.state.contact.title}/>} />
 
               <Footer />
 
