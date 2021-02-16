c52d37071611d652546d9ce56b83a049cdcbf5e4
Joshua <joshuamacleod@live.com>
2021-02-16 15:36:39 +0000

    Updated homePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,15 +3,14 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Carousel from '../components/Carousel';
 import Profile from '../assets/images/profile.png';
-import "/Users/joshua/Makers/Portfolio/src/index.css";
 
 
 function HomePage(props) {
     return(
-        <div> 
+        <div>
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
             <Carousel />
-            <img src={Profile} className="profile-pic" alt="profile-pic" /> 
+            <img src={Profile} className="profile-pic" alt="profile-pic" />
         </div>
 
     );
