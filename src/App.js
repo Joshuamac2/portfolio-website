8064d5378e945ff553d23d15314f4fc6238e3fbb
Joshua <joshuamacleod@live.com>
2021-02-16 00:39:39 +0000

    Links added to navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+
+
+
+
 
 
 
@@ -21,7 +27,7 @@ class App extends React.Component {
       title: 'Joshua Macleod',
       headerLinks: [
         { title: 'Home', path: '/'},
-        { title: 'About', path: '/about'},
+        { title: 'About', path: '/about'}
       ],
       home: {
         title: "Hey, I'm Joshua",
@@ -39,22 +45,21 @@ class App extends React.Component {
     return (
           <Router>
             <Container className="p-0" fluid={true}>
-
               <Navbar className="border-bottom" bg="transparent" expand="lg" >
                 <Navbar.Brand>Joshua Macleod</Navbar.Brand>
-
                 <Navbar.Toggle  className="border-0" aria-controls="navbar-toggle" />
                 <Navbar.Collapse id="navbar-toggle">
                   <Nav className="ml-auto">
                     <Link className="nav-link" to="/">Home</Link>
                     <Link className="nav-link" to="/about">About</Link>
+                    <a className="nav-link" href="https://github.com/Joshuamac2"><FaGithub/></a>
+                    <a className="nav-link" href="https://www.linkedin.com/in/joshua-macleod-8805931a5/"><FaLinkedin/></a>
                   </Nav>
                 </Navbar.Collapse>
               </Navbar>
               <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
               <Route path="/about" render={() => <AboutPage title={this.state.about.title}/>} />
               <Footer />
-
             </Container>
           </Router>
     );
