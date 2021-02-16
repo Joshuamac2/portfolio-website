f541100b8862cfda23ae27b880256cdee057d9a0
Joshua <joshuamacleod@live.com>
2021-02-16 17:45:37 +0000

    Change to about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Hero from '../components/Hero';
 import Content from '../components/Content';
 
 function AboutPage(props) {
     return(
         <div>
-            <Hero title={props.title} />
-
             <Content>
+                <p>My journey,</p>
                 <p>After a postgrad in health sciences and taking some time out for reflection about what I should do next — which consisted of being on a wave somewhere off a beach in Newquay and working
-in various coffee shops in order to fund my passion for traveling and surfing! Due to the pandemic this has obviously blocked all plans in this regard, so to keep myself busy, I looked for a hobby to keep my brain occupied, and I quickly fell in love with the idea of creating stuff, just to amuse myself, even if it was something as basic as a print statement. </p>
+                    in various coffee shops in order to fund my passion for traveling and surfing! Due to the pandemic this has obviously blocked all plans in this regard, so to keep myself busy, I looked for a hobby to keep my brain occupied, and I quickly fell in love with the idea of creating stuff, just to amuse myself, even if it was something as basic as a print statement. </p>
                 <p>Like most developers starting out, where are you supposed to begin? Looking for direction, a friend recommended me to a coding bootcamp to really get the ball rolling and now, have completed just such a course, i have the tools in my bag now to go and build cool things that maybe no one has thought of yet.</p>
                 <p>What am I up to right now? Right now I am building a website aimed at creating a new community, one which will help the average news reader to self-educate and debate both sides of a current political issue and all because I can’t abide the way that social media and algorithms feed and amplify prejudices, of whatever kind. Head back to Home and check out “Not complete” for more info!</p>
-                <p>And If you got to the end of this, thank you, and I hope you enjoyed my website!
-</p>
+                <p>And If you got to the end of this, thank you, and I hope you enjoyed my website!</p>
             </Content>
         </div>
     );
